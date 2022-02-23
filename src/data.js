023b40fecc90280ae0c00605ec6fd64cba54deae
IvBrt <crstn.b7@gmail.com>
2022-02-22 19:17:13 -0600

    filtro por genero

--- src/data.js
+++ src/data.js
@@ -1,14 +1,16 @@
-// estas funciones son de ejemplo
-
 export const drawResults = (character) => {
   return ` <section class="card">
-  <img src="${character.image}" alt="" id="portal">
+  
   <h3>${character.name}</h3>
-</section>`
+  <h3>${character.species}</h3>
+  <h3>${character.gender}</h3>
+  </section>`;
 };
 
+//<img src="${character.image}" alt="" id="portal">
+
 export const anotherExample = () => {
-  return 'OMG';
+  return "OMG";
 };
 
 //crear función con los datos
--- src/main.js
+++ src/main.js
@@ -1,23 +1,80 @@
-import { drawResults } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import { drawResults } from "./data.js";
+import data from "./data/rickandmorty/rickandmorty.js";
 
-//Crear evento del boton
 
 let search = document.querySelector("#search");
-let all = document.getElementById("all");
- function buscar(){
-    let name = search.value;
-    //console.log(name);
- }
+//let all = document.getElementById("all");
 
- search.addEventListener("keyup",buscar)
+function buscar() {
+  let name = search.value;
+  console.log(name);
+}
 
- console.log(data.results)
+search.addEventListener("keyup", buscar);
 
- function getData(data){
-     for(let i=0;i<=data.length;i++){
-         all.innerHTML += drawResults(data[i])
-     }
- }
+console.log(data.results);
 
- getData(data.results);
+// function genderMale(data) {
+//   let male = [];
+
+  // for (let i = 0; i < data.length; i++) {
+  //   var currentMale = data[i];
+  //   if (currentMale == male) {
+  //     all.innerHTML += drawResults(male[i]);
+  //   }
+
+    //************************** FUNCION GENE ******************************
+    // function getData(characterArray) {
+    //   for (let i = 0; i <= characterArray.length-1; i++) {
+    //     console.log(i, characterArray[i])
+    //     all.innerHTML += drawResults(characterArray[i]);
+    //   }
+    // }
+
+    // getData(data.results);
+  
+
+   let filterGender = document.getElementById("gender");
+   let genderSct = document.getElementById("genderSection");
+
+   // lo que queriamos hacer:  let dataRM = data.results.gender
+
+   function btnGender(){ 
+   data.results.forEach(dataRM =>{
+    if(dataRM.gender == 'Male'){
+      genderSct.innerHTML += drawResults(dataRM);  
+    }
+  })
+}
+
+  filterGender.addEventListener("click", btnGender)
+
+  //********** 4 intento***************
+  //function genderFilter (forMale){
+    // for (let i = 0; i <= forMale.length-1; i++){
+    //   // if(forMale[i] == data.results[i])
+    //   // genderSct.innerHTML += drawResults(forMale[i]);
+    // }
+  //}
+  
+  //genderFilter(data.results); 
+
+  //********* 3 intento ***************
+  // let hombre = "male";
+  
+  // const resultado = data.filter(genero => genero.data == hombre);
+  // console.log(drawResults);
+
+  // filterGender.addEventListener("click", resultado);
+
+  //********** 2 intento ***************
+
+  // let male = "male";
+  
+  // function hombre (){
+  //   let filtro = data.filter("male")
+  //   if(filtro == male)
+  //   genderSct.innerHTML = drawResults
+  // }
+
+  // filterGender.addEventListener("click",hombre);
